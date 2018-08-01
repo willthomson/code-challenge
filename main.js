7c2430a38d4376f553325e0bf05ff40401f26851
Will Thomson <will.thomson@gmail.com>
2018-08-01 19:22:55 +0100

    Linting

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
+/* eslint no-magic-numbers: 0 */
 
 /**
  * Each test in the array contains input data and an expected result.
  */
-tests = [
-    {
-        input: [0],
-        result: [0]
-    },
-    {
-        input: [1,2,3],
-        result: [1,2]
-    },
-    {
-        input: 1,
-        result: 1
-    },
-    {
-        input: "hello",
-        result: 'Hello'
-    },
+const tests = [
+  {
+    input: [ 0 ],
+    result: [ 0 ],
+  },
+  {
+    input: [ 1, 2, 3 ],
+    result: [ 1, 2 ],
+  },
+  {
+    input: 1,
+    result: 1,
+  },
+  {
+    input: 'hello',
+    result: 'Hello',
+  },
 ];
 
 /**
  * The function which produces the solution for each test case.
  */
 function solution(input) {
-    return input;
+  return input;
 }
 
 
@@ -36,23 +37,23 @@ const resultsEl = document.querySelector('.results');
 let passes = 0;
 
 tests.forEach((test, index) => {
-    let bgStyle = 'bg-danger';
-    if (JSON.stringify(solution(test.input)) === JSON.stringify(test.result)) {
-        bgStyle = 'bg-success';
-        passes++;
-    }
-    let result = `
-        <div class="result ${bgStyle}">
-            <h4>Test ${index+1}</h4>
-            <strong>Input:</strong> <code>${JSON.stringify(test.input)}</code><br>
-            <strong>Output:</strong> <code>${JSON.stringify(solution(test.input))}</code><br>
-            <strong>Expected:</strong> <code>${JSON.stringify(test.result)}</code>
-        </div>`;
-    resultsEl.innerHTML += result;
+  let bgStyle = 'bg-danger';
+  if (JSON.stringify(solution(test.input)) === JSON.stringify(test.result)) {
+    bgStyle = 'bg-success';
+    passes++;
+  }
+  const result = `
+      <div class="result ${ bgStyle }">
+          <h4>Test ${ index + 1 }</h4>
+          <strong>Input:</strong> <code>${ JSON.stringify(test.input) }</code><br>
+          <strong>Output:</strong> <code>${ JSON.stringify(solution(test.input)) }</code><br>
+          <strong>Expected:</strong> <code>${ JSON.stringify(test.result) }</code>
+      </div>`;
+  resultsEl.innerHTML += result;
 });
 
 resultsEl.innerHTML += `
     <div class="total">
-        <h2>Total:</h2> 
-        Passed ${passes} out of ${tests.length} tests (${Math.round((passes/tests.length)*100)}%)
+        <h2>Total:</h2>
+        Passed ${ passes } out of ${ tests.length } tests (${ Math.round((passes / tests.length) * 100) }%)
     </div>`;
